e7f228ab67c0efe4f28c670fb30ff8eaae4a63cb
Varun Reddy Mogili <mvarunreddy13@gmail.com>
2025-04-03 20:35:32 -0400

    Integrated firestore and made some changes to css

--- api.js
+++ api.js
@@ -1,44 +1,70 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore/lite";
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCeTfYdoStdtkuT-XPBXvaV0Sb2tSVKTvA",
+  authDomain: "van-life-295db.firebaseapp.com",
+  projectId: "van-life-295db",
+  storageBucket: "van-life-295db.firebasestorage.app",
+  messagingSenderId: "614612148945",
+  appId: "1:614612148945:web:376be173dfe9c4bbe85bf0",
+};
 
-export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+export const getVans = async () => {
+  const vansSnapshot = await getDocs(collection(db, "vans"));
+  const vans = vansSnapshot.docs.map((doc) => {
+    return { ...doc.data(), id: doc.id };
+  });
+  return vans;
+};
 
-    return data
-}
+export const getVan = async (id) => {
+  const docRef = doc(db, "vans", id);
+  const docSnap = await getDoc(docRef);
+  console.log(docSnap);
+  if (docSnap.exists()) {
+    return { ...docSnap.data(), id: docSnap.id };
+  } else {
+    throw new Error("No such document!");
+  }
+};
+
+export const getHostVans = async () => {
+  const q = query(collection(db, "vans"), where("hostId", "==", 123));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  return vans;
+};
+
+export const loginUser = async (creds) => {
+  const userSnapshot = await getDocs(collection(db, "user"));
+  const users = userSnapshot.docs.map((doc) => {
+    return { ...doc.data(), id: doc.id };
+  });
+  const foundUser = users.find(
+    (user) => user.email === creds.email && user.password === creds.password
+  );
+  if (!foundUser) {
+    throw {
+      message: "No user with those credentials found!",
+      statusText: "Unauthorized",
+      status: 401,
+    };
+  }
+  return foundUser;
+};
